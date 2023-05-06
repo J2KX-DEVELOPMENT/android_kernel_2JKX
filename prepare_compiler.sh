9fabbe02088d57dfc2da5a5b8e2f7307ce006eda
danya227 <danya2271@yandex.ru>
2023-05-06 10:50:08 +0500

    Use gcc 5.5

--- prepare_compiler.sh
+++ prepare_compiler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://snapshots.linaro.org/gnu-toolchain/13.0-2022.10-1/aarch64-linux-gnu/gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu.tar.xz
-tar -xf gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu.tar.xz
-rm -rf gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu.tar.xz
+wget https://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/aarch64-linux-gnu/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
+tar -xf gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
+rm -rf gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
