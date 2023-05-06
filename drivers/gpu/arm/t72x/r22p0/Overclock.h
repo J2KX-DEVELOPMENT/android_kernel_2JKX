6038d4e6cc567c429c4d763b6291a30ffc8a481b
danya227 <danya2271@yandex.ru>
2023-05-06 15:34:39 +0500

    Gpu overclock macros

---
+++ drivers/gpu/arm/t72x/r22p0/Overclock.h
@@ -0,0 +1,23 @@
+/* drivers/gpu/arm/t72x/r22p0/Overclock.h
+ *
+ * Copyright 2023 by Kuroneko Development
+ *
+ * Samsung overclocking macros
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software FoundatIon.
+ */
+
+#define MAX_FREQ 999
+#define HIGHSPEED_FREQ  666
+#define HIGHSPEED_LOAD 38
+#define MIN_FREQ 80
+#define MAX_FREQ2 999000
+#define MIN_FREQ2 80000
+
+/*
+ * Min frequency shouldn't be higher than 415
+ * Max freq shouldn't be lower than 666
+ * Highspeed freq shouldn't be higher than max freq and lower than 553
+ */
