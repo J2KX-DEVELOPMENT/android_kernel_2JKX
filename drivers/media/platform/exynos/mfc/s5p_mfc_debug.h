24b4382ebe9c6e8e202d312b4e468042d946fc10
danya2271 <danya2271@yandex.ru>
2023-05-08 13:05:48 +0500

    Reduce samsung mfc debugging

--- drivers/media/platform/exynos/mfc/s5p_mfc_debug.h
+++ drivers/media/platform/exynos/mfc/s5p_mfc_debug.h
@@ -17,6 +17,7 @@
 
 #define DEBUG
 
+#ifndef CONFIG_FK_LOG
 #ifdef DEBUG
 extern int debug;
 
@@ -96,5 +97,36 @@ extern int debug;
 			snprintf(dev->mfc_trace[cnt].str, MFC_TRACE_STR_LEN,				\
 					"[c:%d] " fmt, ctx->num, ##args);				\
 		} while(0)
+#else
+
+#define mfc_debug(fmt, args...)	do {} while(0)
+
+#define mfc_debug_enter() 	do {} while(0)
+#define mfc_debug_leave()	do {} while(0)
+
+#define mfc_err(fmt, args...)	do {} while(0)
+
+#define mfc_err_dev(fmt, args...)	do {} while(0)
+
+#define mfc_err_ctx(fmt, args...)	do {} while(0)
+
+#define mfc_info_dev(fmt, args...)	do {} while(0)
+
+#define mfc_info_ctx(fmt, args...)	do {} while(0)
+
+#define MFC_DEV_NUM_MAX			2
+#define MFC_TRACE_STR_LEN		80
+#define MFC_TRACE_COUNT_MAX		1024
+	struct _mfc_trace {
+		unsigned long long time;
+		char str[MFC_TRACE_STR_LEN];
+	};
+
+/* If there is no ctx structure */
+#define MFC_TRACE_DEV(fmt, args...)	do {} while(0)
+
+/* If there is ctx structure */
+#define MFC_TRACE_CTX(fmt, args...)	do {} while(0)
+#endif
 
 #endif /* __S5P_MFC_DEBUG_H */
