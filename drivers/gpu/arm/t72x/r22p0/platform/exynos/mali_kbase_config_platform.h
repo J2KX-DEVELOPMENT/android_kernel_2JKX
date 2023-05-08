109b791331b86d8120ac3cbafe041e162dad8bfa
danya2271 <danya2271@yandex.ru>
2023-05-08 22:28:35 +0500

    maybe fix gpu OC

--- drivers/gpu/arm/t72x/r22p0/platform/exynos/mali_kbase_config_platform.h
+++ drivers/gpu/arm/t72x/r22p0/platform/exynos/mali_kbase_config_platform.h
@@ -29,7 +29,7 @@
  * Attached value: number in kHz
  * Default value: NA
  */
-#define GPU_FREQ_KHZ_MAX (5000)
+#define GPU_FREQ_KHZ_MAX (50000)
 /**
  * Minimum frequency GPU will be clocked at. Given in kHz.
  * This must be specified as there is no default value.
@@ -37,7 +37,7 @@
  * Attached value: number in kHz
  * Default value: NA
  */
-#define GPU_FREQ_KHZ_MIN (5000)
+#define GPU_FREQ_KHZ_MIN (500)
 
 /**
  * CPU_SPEED_FUNC - A pointer to a function that calculates the CPU clock
