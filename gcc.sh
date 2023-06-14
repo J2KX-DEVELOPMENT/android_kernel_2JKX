e1109db44f1b1ba3c0c5751eb882dfa13bedd04b
danya2271 <danya2271@yandex.ru>
2023-06-14 15:12:56 +0500

    Gcc 7.5

--- gcc.sh
+++ gcc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make CROSS_COMPILE=$(pwd)/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- O=out ARCH=arm64 -j40 $1 $2 $3 $4 $5
+make CROSS_COMPILE=/run/media/danya227/8f2b2974-7cde-466e-83cc-a1ab3f620132/gcc-7/bin/aarch64-linux-gnu- O=out ARCH=arm64 -j40 $1 $2 $3 $4 $5
